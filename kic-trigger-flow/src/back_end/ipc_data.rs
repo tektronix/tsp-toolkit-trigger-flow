d4972cac73b26a6dfb1b8623100cb204ae371d9a
mananjeetkaur <mananjeet.kaur@tektronix.com>
2026-02-09 11:42:39 +0530

    remove duplicate ipc_data.rs file

--- kic-trigger-flow/src/back_end/ipc_data.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct IpcData {
-    pub request_type: String,
-    pub additional_info: String,
-    pub json_value: String,
-}
