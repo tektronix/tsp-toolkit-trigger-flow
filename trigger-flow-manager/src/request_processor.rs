c91ddf67cddeb0a43d4751a4dc3881b6d92c2779
mananjeetkaur <mananjeet.kaur@tektronix.com>
2026-02-16 11:24:58 +0530

    fix style errors in requestProcessor

--- trigger-flow-manager/src/request_processor.rs
+++ trigger-flow-manager/src/request_processor.rs
@@ -1,11 +1,13 @@
 use crate::{
-    TriggerBlocks, api::{
-        request::{ RequestType, ResponseType},
+    api::{
+        request::{RequestType, ResponseType},
         state::{SystemConfiguration, TriggerFlowState},
-    }, validator::{ValidationChain, Validator, catalog_validator::CatalogValidator}
+    },
+    validator::{catalog_validator::CatalogValidator, ValidationChain, Validator},
+    TriggerBlocks,
 };
 use anyhow::{Ok, Result};
-use std::{sync::Arc};
+use std::sync::Arc;
 
 pub struct RequestProcessor {
     catalog: Arc<TriggerBlocks>,
@@ -24,23 +26,20 @@ impl RequestProcessor {
             validation_chain,
         }
     }
-    pub fn process_request(
-        &self,
-        request: RequestType,
-        current_state: Option<TriggerFlowState>,
-    ) -> Result<ResponseType> {
+    pub fn process_request(&self, request: RequestType) -> Result<ResponseType> {
         match request {
             RequestType::InitialRequest { system_config } => {
-                self.handle_initial_request(system_config)?;
-                Ok(ResponseType::InitialResponse { system_config, catalog: () })
-            },
+                self.handle_initial_request(system_config.clone())?;
+                Ok(ResponseType::InitialResponse {
+                    system_config: system_config.clone(),
+                    catalog: self.catalog.as_ref().clone(),
+                })
+            }
             RequestType::EvaluateRequest { current_state } => {
-                let state = current_state.ok_or_else(|| {
-                    anyhow::anyhow!("Current state is required for EvaluateRequest")
-                })?;
-                self.handle_evaluate_request(state);
+                let state = current_state.clone();
+                self.handle_evaluate_request(state)?;
 
-                Ok(ResponseType::EvaluateResponse { current_state } )
+                Ok(ResponseType::EvaluateResponse { current_state })
             }
         }
     }
@@ -48,8 +47,13 @@ impl RequestProcessor {
     pub fn handle_initial_request(&self, config: SystemConfiguration) -> Result<()> {
         //validate the config
         //send catalog
+        Ok(())
     }
-    pub fn handle_evaluate_request(&self, current_state: TriggerFlowState) -> Result<{ (state: TriggerFlowState) }> {
+    pub fn handle_evaluate_request(
+        &self,
+        current_state: TriggerFlowState,
+    ) -> Result<TriggerFlowState> {
         //validate against catalog
+        Ok(current_state)
     }
 }
