3a799a62965bad2669e6fee96727019615c4d5e9
rajeshwari-kiwad <rajeshwari.kiwad@tektronix.com>
2026-03-02 10:14:45 +0530

    block parameters component

---
+++ trigger-flow-ui/src/app/main-flow/palette/block-parameters/block-parameters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-parameters',
+  imports: [],
+  templateUrl: './block-parameters.html',
+  styleUrl: './block-parameters.css',
+})
+export class BlockParameters {
+
+}
