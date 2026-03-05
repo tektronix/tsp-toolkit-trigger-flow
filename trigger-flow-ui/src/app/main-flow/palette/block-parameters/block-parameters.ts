e730661f396b7d18c5850c2ce6a837297e4c09d5
rajeshwari-kiwad <rajeshwari.kiwad@tektronix.com>
2026-03-05 11:11:22 +0530

    block-parameter on canvas

--- trigger-flow-ui/src/app/main-flow/palette/block-parameters/block-parameters.ts
+++ trigger-flow-ui/src/app/main-flow/palette/block-parameters/block-parameters.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Textbox } from '../../../custom-controls/textbox/textbox';
+import { Dropdown } from '../../../custom-controls/dropdown/dropdown';
 
 @Component({
   selector: 'app-block-parameters',
-  imports: [],
+  imports: [Textbox, Dropdown],
   templateUrl: './block-parameters.html',
   styleUrl: './block-parameters.css',
 })
 export class BlockParameters {
-
+  name: string = '';
+  branchTo: string = '';
+  notes: string = '';
 }
