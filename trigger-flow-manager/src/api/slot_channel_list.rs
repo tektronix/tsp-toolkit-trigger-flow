0006e0741bde92b60ae39e36a832234f4aa3ec24
mananjeetkaur <mananjeet.kaur@tektronix.com>
2026-04-02 12:09:25 +0530

    change slotChannelList structure

--- trigger-flow-manager/src/api/slot_channel_list.rs
+++ trigger-flow-manager/src/api/slot_channel_list.rs
@@ -24,18 +24,34 @@ pub struct Channel {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Slot {
-    pub slot_index: SlotIndex,
-    pub channels: Vec<Channel>,
+    pub slot_id: SlotIndex,
     pub module: Module,
-    pub node_id: String,
+    pub channels: Vec<Channel>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct SlotJson {
-    #[serde(rename = "slotId")]
     pub slot_id: String,
     pub module: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NodeJson {
+    pub node_id: String,
+    pub mainframe: String,
+    pub slots: Option<Vec<SlotJson>>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Nodes {
+    pub node_id: String,
+    pub mainframe: String,
+    pub slots: Option<Vec<Slot>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -51,6 +67,7 @@ pub struct SystemConfigJson {
     #[serde(rename = "isActive")]
     pub is_active: Option<bool>, // Optionally handle isActive
     pub slots: Option<Vec<SlotJson>>,
+    pub nodes: Option<Vec<NodeJson>>,
 }
 
 #[derive(Debug, Clone)]
@@ -61,12 +78,20 @@ pub enum SlotChannelListUpdate {
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct SlotChannelList {
+    pub localnode: String,
+    pub is_valid: bool,
     pub slots: Vec<Slot>,
+    pub nodes: Vec<Nodes>,
 }
 
 impl Default for SlotChannelList {
     fn default() -> Self {
-        SlotChannelList { slots: Vec::new() }
+        SlotChannelList {
+            localnode: "MP5103".to_string(),
+            is_valid: true,
+            slots: Vec::new(),
+            nodes: Vec::new(),
+        }
     }
 }
 
@@ -74,13 +99,26 @@ impl SlotChannelList {
     pub fn new(system_config_json: &str) -> Result<Self, String> {
         let config_json: SystemConfigJson = serde_json::from_str(system_config_json)
             .map_err(|e| format!("Failed to parse system configuration JSON: {}", e))?;
-        let slots = config_json
+
+        let _slots = config_json
             .slots
             .unwrap_or_default()
             .iter()
-            .map(|slot_json| Slot::try_from((&config_json.localnode, slot_json)))
+            .map(|slot_json| Slot::try_from(slot_json))
             .collect::<Result<Vec<_>, _>>()?;
-        Ok(SlotChannelList { slots })
+
+        let _nodes = config_json
+            .nodes
+            .unwrap_or_default()
+            .iter()
+            .map(|node_json| Nodes::try_from(node_json))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(SlotChannelList {
+            localnode: config_json.localnode,
+            is_valid: true,
+            slots: _slots,
+            nodes: _nodes,})
     }
 
     pub fn update_slot_channel_list(
@@ -91,34 +129,36 @@ impl SlotChannelList {
             SlotChannelListUpdate::SystemConfig(system_config) => {
                 let config_json: SystemConfigJson = serde_json::from_str(&system_config)
                     .map_err(|e| format!("Failed to parse system configuration JSON: {}", e))?;
-                let slots = config_json
+
+                self.slots = config_json
                     .slots
                     .unwrap_or_default()
                     .iter()
-                    .map(|slot_json| Slot::try_from((&config_json.localnode, slot_json)))
+                    .map(|slot_json| Slot::try_from(slot_json))
                     .collect::<Result<Vec<_>, _>>()?;
-
-                self.slots = slots;
             }
             SlotChannelListUpdate::TriggerFlowState(triggerflow_state) => {
                 for slot in &mut self.slots {
                     for channel in &mut slot.channels {
                         channel.in_use = triggerflow_state
-                            .is_channel_in_use(slot.slot_index, channel.channel_index);
+                            .is_channel_in_use(slot.slot_id, channel.channel_index);
                     }
                 }
             }
         }
         Ok(SlotChannelList {
+            localnode: self.localnode.clone(),
+            is_valid: self.is_valid,
             slots: self.slots.clone(),
+            nodes: self.nodes.clone(),
         })
     }
 }
 
-impl TryFrom<(&String, &SlotJson)> for Slot {
+impl TryFrom<&SlotJson> for Slot {
     type Error = String;
 
-    fn try_from((localnode, slot_json): (&String, &SlotJson)) -> Result<Self, Self::Error> {
+    fn try_from(slot_json: &SlotJson) -> Result<Self, Self::Error> {
         let module = match slot_json.module.as_str() {
             "MPSU50-2ST" => Module::MPSU50_2ST,
             "MSMU60-2" => Module::MSMU60_2,
@@ -135,7 +175,8 @@ impl TryFrom<(&String, &SlotJson)> for Slot {
 
         let channel_indices = vec![ChannelIndex(1), ChannelIndex(2)];
         Ok(Slot {
-            slot_index: SlotIndex(slot_index),
+            slot_id: SlotIndex(slot_index),
+            module,
             channels: channel_indices
                 .into_iter()
                 .map(|ci| Channel {
@@ -143,8 +184,33 @@ impl TryFrom<(&String, &SlotJson)> for Slot {
                     in_use: false,
                 })
                 .collect(),
-            module,
-            node_id: localnode.clone(),
+        })
+    }
+}
+
+impl TryFrom<&NodeJson> for Nodes {
+    type Error = String;
+
+    fn try_from(node_json: &NodeJson) -> Result<Self, Self::Error> {
+        let node_id = node_json.node_id.clone();
+        let mainframe = node_json.mainframe.clone();
+
+        // Convert Option<Vec<SlotJson>> to Option<Vec<Slot>>
+        let slots = node_json
+            .slots
+            .as_ref()
+            .map(|slot_json| {
+                slot_json
+                    .iter()
+                    .map(Slot::try_from)
+                    .collect::<Result<Vec<_>, _>>()
+            })
+            .transpose()?;
+
+        Ok(Nodes {
+            node_id,
+            mainframe,
+            slots,
         })
     }
 }
